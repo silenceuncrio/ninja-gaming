6208ee25e09d23303b0a92709b0225f71b0c284a
silenceuncrio <silenceuncrio@gmail.com>
2022-02-20 21:07:56 +0800

    initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<div class="about">
+  <h2>About Us</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae molestiae cupiditate laboriosam repellat eligendi. A esse neque, ullam dolorem voluptates, eius facere nesciunt commodi assumenda voluptas inventore, error tenetur eum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque totam eum, debitis facere eligendi ratione est, neque corporis fugit, rem similique nemo libero itaque accusamus mollitia deleniti ullam eius dolore?</p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum ex ipsum quod minima assumenda corporis, iure possimus reprehenderit laudantium, autem aliquam excepturi quae reiciendis, amet odit magnam sit voluptatibus accusantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere minima ad sed debitis voluptates labore veniam blanditiis esse vel iusto ducimus, repellendus amet ea ullam corrupti iste tempore explicabo totam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aliquam dicta provident suscipit praesentium asperiores a assumenda porro quidem sapiente, et sit? Ad ducimus sequi magni at maiores nemo saepe?</p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
